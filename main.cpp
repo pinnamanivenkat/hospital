fff5243f1005d1c950d23de7e400980782ed5a9f
pinnamanivenkat <pinnamanivenkat@gmail.com>
2016-10-19 20:02:13 +0530

    Update

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#inlcude<ctime>
+using namespace std;
+int main()
+{
+	int a;
+/*    string env,path;
+    env = getenv("HOME");
+    path=env+modhos+display*;*/s
+	system("~/modhos/display.sh");
+	cin>>a;
+	patient pnt;
+	switch(a)
+	{
+		case 1:
+			pnt.nwpnt();
+	};
+}
+
+
+
